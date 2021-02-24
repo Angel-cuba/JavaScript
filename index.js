1960f54aa04b89e9ee222f239ee67b382783680c
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-24 23:45:55 +0200

    Operador ternario

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const number = document.getElementById('number')
-const result = document.getElementById('result')
+ const number = document.getElementById('number')
+// const result = document.getElementById('result')
+
 
 let a = prompt('Enter primer numero:')
 let b = prompt('Enter segundo:')
 let c = prompt('Enter tercero:')
 
-number.innerHTML = `Los numeros son ${a}, ${b}, ${c}`
-number.textContent = `Los numeros son ${a}, ${b}, ${c}`
+// number.innerHTML = `Los numeros son ${a}, ${b}, ${c}`
+// number.textContent = `Los numeros son ${a}, ${b}, ${c}`
+
+let result = a+b+c;
+
+// if (result % 2 == 0) console.log(`${result} es par`);
+// else console.log(`${result} es impar`)
+
+(result % 2 == 0) ? (
+          console.log(`${result} es par`),
+           number.textContent = `Los numeros son ${a}, ${b}, ${c}, y el numero es ${result}`
+           )
+           :
+            console.log(`${result} es impar`)
