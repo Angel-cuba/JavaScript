b518bc1aa2a8a39e468a6f05ad6bcc2436449cef
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-24 04:30:35 +0200

    Testing

---
+++ index.js
@@ -0,0 +1,9 @@
+const number = document.getElementById('number')
+const result = document.getElementById('result')
+
+
+let a = prompt('Enter primer numero:')
+let b = prompt('Enter segundo:')
+let c = prompt('Enter tercero:')
+
+number.innerHTML = `Los numeros son ${a}, ${b}, ${c}`
