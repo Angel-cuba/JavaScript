b0cce588425cadff10f608d5d5f6a0168ffdd54a
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-25 15:37:11 +0200

    Update

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
  const number = document.getElementById('number')
-// const result = document.getElementById('result')
+ const results = document.getElementById('result')
+const btn = document.getElementById('btn')
+const menu = document.getElementById('menu')
 
-
-let a = prompt('Enter primer numero:')
-let b = prompt('Enter segundo:')
-let c = prompt('Enter tercero:')
+// let a = prompt('Enter primer numero:')
+// let b = prompt('Enter segundo:')
+// let c = prompt('Enter tercero:')
 
 // number.innerHTML = `Los numeros son ${a}, ${b}, ${c}`
 // number.textContent = `Los numeros son ${a}, ${b}, ${c}`
 
-let result = a+b+c;
+
+// let result = a+b+c;
 
 // if (result % 2 == 0) console.log(`${result} es par`);
 // else console.log(`${result} es impar`)
 
-(result % 2 == 0) ? (
-          console.log(`${result} es par`),
-           number.textContent = `Los numeros son ${a}, ${b}, ${c}, y el numero es ${result}`
-           )
-           :
-            console.log(`${result} es impar`)
+// (result % 2 == 0) ? (
+//           console.log(`${result} es par`),
+//            number.textContent = `Los numeros son ${a}, ${b}, ${c}, y el numero es ${result}`
+//            )
+//            :
+//             console.log(`${result} es impar`)
+
+btn.addEventListener('click', function(e) {
+//     menu.classList.add('.menu1')
+//     menu.classList.remove('.menu1')
+
+console.log('click')
+})
