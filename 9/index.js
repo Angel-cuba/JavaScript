5c8185366f7dec79b85e588074bbd1576a08028c
Angel-cuba <araozangel842@gmail.com>
2021-03-01 21:29:05 +0200

    Testing new name in Gitdesktop

---
+++ 9/index.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById('btn')
+
+
+addEventListener('click', (e) => {
+     console.log(e)
+})
+
+console.log(btn)
+
+const yo = {
+     name: 'Angel',
+     edad: 36,
+     email: 'angel@gmail.com'
+}
+
+console.log(yo)
+console.log(location.href)
+console.log(location.protocol)
+console.log(location.host)
+console.log(location.hostname)
+console.log(location.pathname)
+console.log(location.hash)
+
+//location.reload() 
+//location.href = 'http://google.com'
+
+
