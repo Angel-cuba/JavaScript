e96786e3ce99aa9dce8f121960592ba59439b27e
Angel-cuba <araozangel842@gmail.com>
2021-03-02 17:41:36 +0200

    Obtener datos mediante AJAX (XMLHttpRequest)

---
+++ AJAX/index.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+
+let xhr
+
+if (window.XMLHttpRequest) xhr = new XMLHttpRequest();
+     else xhr = new ActiveXObject("Microsoft.XMLHTTP");
+
+
+     xhr.open("GET",'https://jsonplaceholder.typicode.com/users')
+     
+     xhr.addEventListener('load', (data) => {
+        console.log(JSON.parse(data.target.response))
+        const midata = JSON.parse(data.target.response)
+        console.log(midata)
+
+        const list = document.getElementById('list')
+      for(const user of midata){
+const listItem = document.createElement('li')
+listItem.textContent = `${user.id} - ${user.name}`
+
+list.appendChild(listItem)
+      }
+     })
+     
+     
+     xhr.send();
+})
