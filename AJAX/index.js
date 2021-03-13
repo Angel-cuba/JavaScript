1455449533299a03887eac154c423ece5756268e
Angel-cuba <araozangel842@gmail.com>
2021-03-13 17:45:56 +0200

    Adding example .html

--- AJAX/index.js
+++ AJAX/index.js
@@ -1,30 +1,27 @@
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
+  let xhr;
+  // testing a test with testing
 
-let xhr
-// testing a test with testing
+  if (window.XMLHttpRequest) xhr = new XMLHttpRequest();
+  else xhr = new ActiveXObject('Microsoft.XMLHTTP');
 
-if (window.XMLHttpRequest) xhr = new XMLHttpRequest();
-     else xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
 
+  xhr.addEventListener('load', (data) => {
+    console.log(JSON.parse(data.target.response));
+    const midata = JSON.parse(data.target.response);
+    console.log(midata);
 
-     xhr.open("GET",'https://jsonplaceholder.typicode.com/users')
-     
-     xhr.addEventListener('load', (data) => {
-        console.log(JSON.parse(data.target.response))
-        const midata = JSON.parse(data.target.response)
-        console.log(midata)
+    const list = document.getElementById('list');
+    for (const user of midata) {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${user.id} - ${user.name}`;
 
-        const list = document.getElementById('list')
-      for(const user of midata){
-const listItem = document.createElement('li')
-listItem.textContent = `${user.id} - ${user.name}`
+      list.appendChild(listItem);
+    }
+  });
 
-list.appendChild(listItem)
-      }
-     })
-     
-     
-     xhr.send();
-})
+  xhr.send();
+});
