66411af83e43a4b0c59b1a966ced7ec636b9bbb2
Angel Luis <araozangel842@gmail.com>
2021-04-24 16:28:09 +0300

    Form, css y su js

---
+++ Form/index.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form');
+const button = document.getElementById('submit')
+
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const gender = document.getElementById('gender')
+const terms = document.getElementById('terms')
