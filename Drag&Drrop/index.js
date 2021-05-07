22101be6ceca023f218bf2c277cf95de2561682a
Angel Luis <araozangel842@gmail.com>
2021-05-08 01:53:01 +0300

    drag y menu

---
+++ Drag&Drrop/index.js
@@ -0,0 +1,8 @@
+const pendingTasks = document.getElementById('pending-tasks')
+const finishedTasks = document.getElementById('finished-tasks')
+
+pendingTasks.addEventListener('click', (e)=>{
+     // e.preventDefault();
+     console.log(e.target.id)
+       //dataTransfer
+})
